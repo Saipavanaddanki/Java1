6f33b11075fdf7c48bdbcec0f1932b36479c2429
sai <addanki.saipavan@gmail.com>
2024-02-28 18:53:40 +0530

    Pl-committing-01

---
+++ Maven_Example03/src/main/java/Nit/Maven_Example03/App.java
@@ -0,0 +1,20 @@
+package Nit.Maven_Example03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int k=50;
+        int l=60;
+       App. mul(k,l);
+       
+    }
+    public static void mul(int k,int l){
+    	System.out.println(k*l);
+    }
+	
+}
